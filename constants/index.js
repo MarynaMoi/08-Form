276f2ac42f7e0586dd59d7af40e2d58762325c6e
Maryna <marinakukusua@gmail.com>
2025-12-10 21:54:28 +0200

    Add email and password validation for HW26

--- constants/index.js
+++ constants/index.js
@@ -1,26 +1,97 @@
 export const inputs = [
-  { type: "text", placeholder: "First name", className: "form-input", id: "firstName" },
-  { type: "text", placeholder: "Last name", className: "form-input", id: "lastName" },
-  { type: "text", placeholder: "Display name", className: "form-input", id: "displayName" },
-  { type: "email", placeholder: "Email Address", className: "form-input", id: "email" },
-  { type: "password", placeholder: "Password", className: "form-input" },
+  {
+    type: "text",
+    placeholder: "First name",
+    className: "form-input",
+    name: "firstName",
+  },
+  {
+    type: "text",
+    placeholder: "Last name",
+    className: "form-input",
+    name: "lastName",
+  },
+  {
+    type: "text",
+    placeholder: "Display name",
+    className: "form-input",
+    name: "displayName",
+  },
+  {
+    type: "email",
+    placeholder: "Email Address",
+    className: "form-input",
+    name: "email",
+    id: "email",
+  },
+  {
+    type: "password",
+    placeholder: "Password",
+    className: "form-input",
+    name: "password",
+    id: "password",
+  },
   {
     type: "password",
     placeholder: "Password Confirmation",
     className: "form-input",
+    id: "passwordConfirmation",
+    name: "password",
   },
 ];
 
 export const radioInputs = [
-  { type: "radio", name: "buyer-or-seller", id: "buyer" },
-  { type: "radio", name: "buyer-or-seller", id: "seller" },
+  {
+    type: "radio",
+    name: "buyer-or-seller",
+    id: "buyer",
+    text: "Join As a Buyer",
+    description:
+      "I am looking for a Name, Logo or Tagline for my business, brand or product.",
+  },
+  {
+    type: "radio",
+    name: "buyer-or-seller",
+    id: "seller",
+    text: "Join As a Creative or Marketplace Seller",
+    description:
+      "I plan to submit name ideas, Logo design or sell names in Domain Marketplace.",
+  },
 ];
 export const checkboxInputs = [
   {
     type: "checkbox",
     className: "checkbox-btn",
     id: "checkbox-allow-send-marketing-btn",
+    text: "Allow Squadhelp to send marketing/promotional offers from time to time",
   },
 ];
 
+export const buttonContent = {
+  type: "submit",
+  textContent: "Create Account",
+};
 
+export const formContent = {
+  title: { textContent: "CREATE AN ACCOUNT" },
+  subtitle: {
+    textContent: "We always keep your name and email address private.",
+    className: "head-p",
+  },
+  formInputsDiv: { className: "form-inputs-div" },
+  formRadioDiv: { className: "radios-div" },
+};
+
+export const errorContent = {
+  errorMesConteiner: { id: "error" },
+  emailError: {
+    id: "email-error",
+    className: "error-message",
+    textContent: "Invalid email address.",
+  },
+  passwordConfirmationError: {
+    id: "password-error",
+    className: "error-message",
+    textContent: "Invalid password confirmation",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,108 +1,145 @@
 "use strict";
-import { inputs, radioInputs, checkboxInputs} from "../constants/index.js";
+import {
+  inputs,
+  radioInputs,
+  checkboxInputs,
+  buttonContent,
+  formContent,
+  errorContent,
+} from "../constants/index.js";
+
 const [buyer, seller] = radioInputs;
 const [checkboxMarketing] = checkboxInputs;
 
-function createInput(props) {
-  const input = document.createElement("input");
+function createEl(tag, props) {
+  const el = document.createElement(tag);
   for (const key in props) {
-    input[key] = props[key];
+    if (key === "description" || key === "text") continue;
+    el[key] = props[key];
   }
-  return input;
+  return el;
 }
 
-function createRadioOption(el, text, desc) {
+function createRadioOption(el) {
   const label = document.createElement("label");
   label.className = "label-radio-area";
   label.htmlFor = el.id;
-  const btn = createInput(el);
+  const btn = createEl("input", el);
   const textRadioBtn = document.createElement("span");
-  textRadioBtn.textContent = text;
+  textRadioBtn.textContent = el.text;
   const textDescription = document.createElement("span");
   textDescription.className = "radio-description";
-  textDescription.textContent = desc;
+  textDescription.textContent = el.description;
   label.append(btn, textRadioBtn, textDescription);
   return label;
 }
 
-function createCheckbox(el, text) {
+function createCheckbox(el) {
   const checkboxLabel = document.createElement("label");
   checkboxLabel.className = "check-div";
   checkboxLabel.htmlFor = el.id;
-  const checkbox = createInput(el);
+  const checkbox = createEl("input", el);
   const checkboxText = document.createElement("span");
-  checkboxText.textContent = text;
+  checkboxText.textContent = el.text;
   checkboxText.className = "check-div check-span";
   checkboxLabel.append(checkbox, checkboxText);
   return checkboxLabel;
 }
 
+/* -------------------Create Element and Append Element--------------------*/
 const main = document.createElement("main");
 const form = document.createElement("form");
-
-const h1 = document.createElement("h1");
-h1.textContent = "CREATE AN ACCOUNT";
-
-const p = document.createElement("p");
-p.className = "head-p";
-p.textContent = "We always keep your name and email address private.";
-
-const inputDiv = document.createElement("div");
-inputDiv.className = "form-inputs-div";
-
-const radioDiv = document.createElement("div");
-radioDiv.className = "radios-div";
-
-radioDiv.append(
-  createRadioOption(
-    buyer,
-    "Join As a Buyer ",
-    "I am looking for a Name, Logo or Tagline for my business, brand or product."
-  )
-);
-radioDiv.append(
-  createRadioOption(
-    seller,
-    "Join As a Creative or Marketplace Seller ",
-    "I plan to submit name ideas, Logo design or sell names in Domain Marketplace."
-  )
-);
-
-const checkboxLabel = createCheckbox(
-  checkboxMarketing,
-  "Allow Squadhelp to send marketing/promotional offers from time to time"
-);
-
-const button = document.createElement("button");
-button.type = "submit";
-button.textContent = "Create account";
+const h1 = createEl("h1", formContent.title);
+const p = createEl("p", formContent.subtitle);
+const inputDiv = createEl("div", formContent.formInputsDiv);
+const radioDiv = createEl("div", formContent.formRadioDiv);
+const checkboxLabel = createCheckbox(checkboxMarketing);
+const button = createEl("button", buttonContent);
+const errorContainer = createEl("div", errorContent.errorMesConteiner);
 
 document.body.append(main);
 main.append(form);
 form.append(h1, p, inputDiv, radioDiv, checkboxLabel, button);
+radioDiv.append(createRadioOption(buyer));
+radioDiv.append(createRadioOption(seller));
 inputs.forEach((cfg) => {
-  inputDiv.append(createInput(cfg));
+  inputDiv.append(createEl("input", cfg));
 });
+form.append(errorContainer);
 
-
-
+/* ------------------------Local Storage-------------------------*/
+const propsArray = Array.from(
+  document.querySelectorAll(".form-inputs-div>input")
+);
 class Person {
-  constructor(firstName, lastName, displayName, email) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.displayName = displayName;
-    this.email = email;
+  constructor(...args) {
+    args.forEach(({ name, value }) => (this[name] = value));
   }
 }
 
-button.addEventListener("click", function (e) {
+function saveToLocalStorage() {
+  const person = new Person(...propsArray);
+  localStorage.setItem(
+    `${person.lastName}`,
+    JSON.stringify(
+      person,
+      (key, value) => {
+        return key === "email" || key === "password" ? undefined : value;
+      },
+      2
+    )
+  );
+}
+/* ----------------------------Validation--------------------------*/
 
-  const firstName = document.getElementById("firstName").value.trim();
-  const lastName = document.getElementById("lastName").value.trim();
-  const displayName = document.getElementById("displayName").value.trim();
-  const email = document.getElementById("email").value.trim();
+function addError(el) {
+  if (document.getElementById(el.id)) return;
+  errorContainer.append(createEl("div", el));
+}
 
-  const person = new Person(firstName, lastName, displayName, email);
-  localStorage.setItem(lastName, JSON.stringify(person));
-  form.reset();
-});
+function removeError(id) {
+  const el = document.getElementById(id);
+  if (el) {
+    el.remove();
+  }
+}
+
+function blinkError(el) {
+  el.classList.add("blink");
+  setTimeout(() => el.classList.remove("blink"), 150);
+}
+
+function validatePassword() {
+  const passwordConfirmation = document.getElementById("passwordConfirmation");
+  const password = document.getElementById("password");
+  if (password.value.trim() === "") {
+    addError(errorContent.passwordConfirmationError);
+    return;
+  } else {
+    removeError(errorContent.passwordConfirmationError.id);
+  }
+
+  password.value.trim() === passwordConfirmation.value.trim()
+    ? removeError(errorContent.passwordConfirmationError.id)
+    : addError(errorContent.passwordConfirmationError);
+}
+function validateEmail() {
+  const email = document.getElementById("email");
+  const pattern = /^\w+\.?\w+@[a-z]{3,}\.[a-z]{2,}$/i;
+  pattern.test(email.value.trim())
+    ? removeError(errorContent.emailError.id)
+    : addError(errorContent.emailError);
+}
+function submitHandler(e) {
+  validateEmail();
+  validatePassword();
+  if (errorContainer.children.length > 0) {
+    e.preventDefault();
+    Array.from(errorContainer.children).forEach(blinkError);
+    return;
+  }
+  saveToLocalStorage();
+}
+email.addEventListener("blur", validateEmail);
+passwordConfirmation.addEventListener("blur", validatePassword);
+form.addEventListener("submit", submitHandler);
